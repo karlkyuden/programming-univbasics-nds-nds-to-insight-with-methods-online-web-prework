ad18872ea4704fe757edbc3253a98826b08a5bce
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-22 02:34:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,5 +14,19 @@ end
 # using director_data as input
 def gross_for_director(director_data)
   
+  while row_index < directors_database.length do
+    column_index = 0
+    grandTotal = 0
+    while column_index < directors_database[row_index][:movies].length do
+      grandTotal +=directors_database[row_index][:movies][column_index][:worldwide_gross]
+      column_index += 1
+    end
+    
+    director = directors_database[row_index][:name]
+    newHash[director] = grandTotal
+    row_index += 1
+  end
+  return newHash
   
 end
+
