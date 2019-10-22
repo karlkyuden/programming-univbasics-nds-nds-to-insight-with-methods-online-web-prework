dce75020ad406557b59fca35ed8161095d73f1b8
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-22 02:43:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,7 @@ def directors_totals(nds)
   result = {}
   row_index = 0
   while row_index < nds.length do
-    result[nds[row_index][:name]] = gross_for_director(nds[row_index[:name]])
+    puts gross_for_director(nds[row_index[:name]])
     row_index +=1
   end
   return result
