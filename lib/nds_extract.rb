2c47a1f53b642a82712481498dfe613fb42c9fd2
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-22 02:49:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,6 +19,7 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  puts director_data
   grandTotal = 0
   column_index = 0
     while column_index < director_data.length do
